f8521223fb497867960105f758f66839fcd8ec04
d.bolkunov <d.bolkunov@kt-team.de>
2022-02-03 22:37:27 +0400

    init commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rs-integration/trj
+
+go 1.17
---
+++ job.go
@@ -0,0 +1,11 @@
+package lreChekInvoiceCirculationStatusInKorusJob
+
+import "fmt"
+
+const (
+	JOB_HEADER string = "L'oreal check invoice circulation status"
+)
+
+func run() {
+	fmt.Print(JOB_HEADER)
+}
